b330236487f0c5fbcb0f3df23107a2e065ae023a
Azizbek <azizbektulqinov32@gmail.com>
2024-03-27 13:32:40 +0500

    new change

--- main.js
+++ main.js
@@ -1,26 +1,64 @@
-let hint = prompt("matematik ishorani kiriting 🥸");
-let num1 = +prompt("birinchi sonni kiriting");
-let num2 = +prompt("ikkinchi sonni kiriting");
+// let hint = prompt("matematik ishorani kiriting 🥸");
+// let num1 = +prompt("birinchi sonni kiriting");
+// let num2 = +prompt("ikkinchi sonni kiriting");
 
-function calc(opp, num3, num4) {
-  let calc;
-  switch (opp) {
-    case "+":
-      calc = +num3 + +num4;
-      return calc;
-    case "-":
-      calc = num3 - num4;
-      return calc;
-    case "*":
-      calc = num3 * num4;
-      return calc;
-    case "/":
-      calc = num3 / num4;
-      return calc;
-    default:
-      calc = "hatolik bor tekshiring";
-      return calc;
+// function calc(opp, num3, num4) {
+//   let calc;
+//   switch (opp) {
+//     case "+":
+//       calc = +num3 + +num4;
+//       return calc;
+//     case "-":
+//       calc = num3 - num4;
+//       return calc;
+//     case "*":
+//       calc = num3 * num4;
+//       return calc;
+//     case "/":
+//       calc = num3 / num4;
+//       return calc;
+//     default:
+//       calc = "hatolik bor tekshiring";
+//       return calc;
+//   }
+// }
+// let result = calc(hint, num1, num2);
+// alert(result);
+
+// function user(name, age) {
+//   let tr;
+//   if (age > 18) {
+//     tr = true;
+//   } else if (age < 18) {
+//     tr = false;
+//   }
+//   let obj = {
+//     name,
+//     age,
+//     gr: tr,
+//   };
+//   return obj;
+// }
+
+// console.log(user("Azizbek", 15));
+
+function login() {
+  if (email === "@azizbek" && password === 12345678) {
+    alert("Tabriklayman siz ruyhatdan utdingiz");
+  } else {
+    alert("Iltimos, yana bir bor urinib ko'ring.");
   }
 }
-let result = calc(hint, num1, num2);
-alert(result);
+let count = 1;
+while (count <= 3) {
+  let email = prompt("Emailni kiriting:");
+  let password = prompt("parolni kiriting:");
+  if (email === "@azizbek" && password === "12345678") {
+    alert("Tabriklayman siz ruyhatdan utdingiz");
+    break;
+  } else {
+    alert("Iltimos, yana bir bor urinib ko'ring.");
+  }
+  count++;
+}
+email;
